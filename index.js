38c3ca1e9415eabeaee33aaf6d0e6caf371686ce
github username <github email address>
2021-05-07 02:15:50 -0400

    worked with getters and setters alongside extending classes

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+class Polygon {
+    constructor(parameterInArray){
+        this.parameter = parameterInArray;
+    }
+
+    get countSides(){
+        return this.parameter.length;
+    }
+
+    get perimeter(){
+        return this.parameter.reduce((acc, current) => acc += current)
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid(){
+        const sidesSorted = this.parameter.sort((a,b) => a - b); 
+        
+        return (sidesSorted[0] + sidesSorted[1] > sidesSorted[2] && this.countSides === 3);
+    }
+}
+
+class Square extends Polygon {
+    get isValid(){
+        return (this.countSides === 4 && this.perimeter / this.parameter[0] === 4)
+    }
+
+    get area(){
+        return Math.pow(this.parameter[0], 2)
+    }
+}
